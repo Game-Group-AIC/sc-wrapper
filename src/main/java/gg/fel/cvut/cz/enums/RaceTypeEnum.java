b5c2e5003526baebfdec2b1ba4981964b6edca07
Michal Sustr <michal.sustr@blindspot.ai>
2017-12-08 16:55:58 +0100

    Add tech types

--- src/main/java/gg/fel/cvut/cz/enums/RaceTypeEnum.java
+++ src/main/java/gg/fel/cvut/cz/enums/RaceTypeEnum.java
@@ -1,6 +1,8 @@
 package gg.fel.cvut.cz.enums;
 
 import bwapi.Race;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public enum RaceTypeEnum implements IGameTypes<Race, RaceTypeEnum> {
@@ -21,5 +23,53 @@ public enum RaceTypeEnum implements IGameTypes<Race, RaceTypeEnum> {
     return RaceTypeEnum.values();
   }
 
-  // todo: static set of tech possibilities here from IRace
+  final private static HashMap<RaceTypeEnum, HashSet<TechTypeEnum>> techTypes = new HashMap<>();
+  static {
+    HashSet<TechTypeEnum> zergTech = new HashSet<>();
+    zergTech.add(TechTypeEnum.Burrowing);
+    zergTech.add(TechTypeEnum.Infestation);
+    zergTech.add(TechTypeEnum.SpawnBroodlings);
+    zergTech.add(TechTypeEnum.DarkSwarm);
+    zergTech.add(TechTypeEnum.Plague);
+    zergTech.add(TechTypeEnum.Consume);
+    zergTech.add(TechTypeEnum.Ensnare);
+    zergTech.add(TechTypeEnum.Parasite);
+    zergTech.add(TechTypeEnum.LurkerAspect);
+    techTypes.put(Zerg, zergTech);
+
+    HashSet<TechTypeEnum> terranTech = new HashSet<>();
+    terranTech.add(TechTypeEnum.StimPacks);
+    terranTech.add(TechTypeEnum.Lockdown);
+    terranTech.add(TechTypeEnum.EMPShockwave);
+    terranTech.add(TechTypeEnum.SpiderMines);
+    terranTech.add(TechTypeEnum.ScannerSweep);
+    terranTech.add(TechTypeEnum.TankSiegeMode);
+    terranTech.add(TechTypeEnum.DefensiveMatrix);
+    terranTech.add(TechTypeEnum.Irradiate);
+    terranTech.add(TechTypeEnum.YamatoGun);
+    terranTech.add(TechTypeEnum.CloakingField);
+    terranTech.add(TechTypeEnum.PersonnelCloaking);
+    terranTech.add(TechTypeEnum.Restoration);
+    terranTech.add(TechTypeEnum.OpticalFlare);
+    terranTech.add(TechTypeEnum.Healing);
+    terranTech.add(TechTypeEnum.NuclearStrike);
+    techTypes.put(Terran, terranTech);
+
+    HashSet<TechTypeEnum> protossTech= new HashSet<>();
+    protossTech.add(TechTypeEnum.PsionicStorm);
+    protossTech.add(TechTypeEnum.Hallucination);
+    protossTech.add(TechTypeEnum.Recall);
+    protossTech.add(TechTypeEnum.StasisField);
+    protossTech.add(TechTypeEnum.ArchonWarp);
+    protossTech.add(TechTypeEnum.DisruptionWeb);
+    protossTech.add(TechTypeEnum.MindControl);
+    protossTech.add(TechTypeEnum.DarkArchonMeld);
+    protossTech.add(TechTypeEnum.Feedback);
+    protossTech.add(TechTypeEnum.Maelstrom);
+    techTypes.put(Protoss, protossTech);
+  }
+
+  public static HashSet<TechTypeEnum> getTechType(RaceTypeEnum race) {
+    return techTypes.get(race);
+  }
 }
